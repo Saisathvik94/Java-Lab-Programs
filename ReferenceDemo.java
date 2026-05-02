a67695ebab992f480a533f3529c9f645663efa63
Sai Sathvik <saisathvik615@gmail.com>
2026-05-02 13:25:38 +0530

    Call By Reference

---
+++ ReferenceDemo.java
@@ -0,0 +1,16 @@
+
+import java.lang.ref.Reference;
+
+class ReferenceDemo {
+    int x = 100;
+    void display (ReferenceDemo R) {
+        R.x = R.x + 100;
+    }
+
+    public static void main(String[] args) {
+        ReferenceDemo r = new ReferenceDemo();
+        System.out.println("Before Calling: "+ r.x);
+        r.display(r);
+        System.out.println("After Calling: "+ r.x);
+    }
+}

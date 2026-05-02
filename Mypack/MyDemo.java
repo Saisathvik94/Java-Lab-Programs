c247129d97dc2cef9293d2817d7bf3e0b945b6b9
Sai Sathvik <saisathvik615@gmail.com>
2026-05-02 19:58:54 +0530

    MyPack Demo

---
+++ Mypack/MyDemo.java
@@ -0,0 +1,13 @@
+package Mypack;
+
+public class MyDemo {
+    public void display() {
+        System.out.println("Method 1");
+    }
+    public void call1() {
+        System.out.println("Method 2");
+    }
+    public void call2() {
+        System.out.println("Method 3");
+    }
+}
---
+++ PackDemo.java
@@ -0,0 +1,10 @@
+import Mypack.MyDemo;
+
+public class PackDemo {
+    public static void main(String[] args) {
+        MyDemo d = new MyDemo();
+        d.display();
+        d.call1();
+        d.call2();
+    }
+}
